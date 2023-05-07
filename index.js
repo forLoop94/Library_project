fc5efd195f5bec9f459889f7d7c9a7416303fc1a
forLoop94 <charlesuwem94@gmail.com>
2023-05-07 10:54:24 +0100

    Create function for adding books to target genres

--- index.js
+++ index.js
@@ -27,6 +27,25 @@ genreForm.addEventListener('submit', () => {
   render();
 })
 
+const addBook = (title, author, id) => {
+  function Book(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  const arr = JSON.parse(localStorage.getItem('libraryCollection'));
+
+  const newBook = new Book(title, author);
+
+  for (let i = 0; i < arr.length; i++) {
+    let single = arr[i];
+    if(single.name === id) {
+      single.books = single.books.concat(newBook);
+      localStorage.setItem('libraryCollection', JSON.stringify(arr));
+    }
+  } 
+}
+
 const render = () => {
   const arr = JSON.parse(localStorage.getItem('libraryCollection'));
   genreDisplay.innerHTML = '';
