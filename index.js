f020817d5891c9ef042bc2ca7d5f72846fad548a
forLoop94 <charlesuwem94@gmail.com>
2023-05-07 10:56:38 +0100

    Create display function for displayingg books inside target genres

--- index.js
+++ index.js
@@ -46,6 +46,23 @@ const addBook = (title, author, id) => {
   } 
 }
 
+const display = (id) => {
+  const arr = JSON.parse(localStorage.getItem('libraryCollection'));
+  displaySection.innerHTML = '';
+  for (let i = 0; i < arr.length; i++) {
+    const selectedGenre = arr[i];
+    if (selectedGenre.name === id) {
+      for (let j = 0; j < selectedGenre.books.length; j++) {
+        const selectedBook = selectedGenre.books[j];
+        const bookContainer = document.createElement('div');
+        bookContainer.className = 'book-container';
+        bookContainer.innerHTML = `<div class='book-div flex'><div>${selectedBook.title} by ${selectedBook.author}</div><buton class='remove-book'>Remove</buton><div>`;
+        displaySection.appendChild(bookContainer);
+      }
+    }
+  }
+}
+
 const render = () => {
   const arr = JSON.parse(localStorage.getItem('libraryCollection'));
   genreDisplay.innerHTML = '';
